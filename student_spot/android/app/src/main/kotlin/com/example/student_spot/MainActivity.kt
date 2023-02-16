574bb0162262c89bfd53b8d993971ecc1338f8f6
NolanMlb <nolanmlb@outlook.fr>
2023-02-16 21:12:49 +0100

    add logo

--- student_spot/android/app/src/main/kotlin/com/example/student_spot/MainActivity.kt
+++ student_spot/android/app/src/main/kotlin/com/example/student_spot/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.student_spot
+package com.fablab.student_spot
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ student_spot/lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 120,
+        color: Colors.white,
+        child: Container(
+          margin: const EdgeInsets.only(top: 50.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  'assets/img/logo.png',
+                  height: 30,
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ));
+  }
+}
